e672d5932aa5c0082c05b0e86605fec35fa78378
Cristiano Betta <cbetta@gmail.com>
2011-04-22 18:17:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gwphotos::Application.config.secret_token = '741832298eb68cb592c51d86874a0f86a7d75d16ca269e8f47626a86845ae49876772f0b67bc157a3ede6693f024c4fcc9d44fd33ca9d9c888c4fd6050227054'
