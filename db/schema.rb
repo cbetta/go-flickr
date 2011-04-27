a4359fbba288e04f1fe0de8954000db415c16310
Cristiano Betta <cbetta@gmail.com>
2011-04-27 22:12:49 +0200

    Added delayed job migration
    Signed-off-by: Cristiano Betta <cbetta@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110427162249) do
+ActiveRecord::Schema.define(:version => 20110427201220) do
 
   create_table "authentications", :force => true do |t|
     t.string   "provider"
@@ -21,6 +21,21 @@ ActiveRecord::Schema.define(:version => 20110427162249) do
     t.string   "access_token"
     t.string   "username"
   end
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "photos", :force => true do |t|
     t.string   "url"
