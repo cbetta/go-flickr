3d33c1f7a702f814709dd212a86d680e9592c912
Cristiano Betta <cbetta@gmail.com>
2011-04-27 21:10:55 +0200

    Marked photos as uploaded and deleted temp files
    Signed-off-by: Cristiano Betta <cbetta@gmail.com>

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,12 +7,14 @@ class Photo < ActiveRecord::Base
   # Uploads the photos to flickr and marks the model as uploaded
   # 
   def upload
+    # config flickr raw
     FlickRaw.api_key=ENV['GWPHOTOS_FLICKR_KEY']
     FlickRaw.shared_secret=ENV['GWPHOTOS_FLICKR_SECRET']
+    flickr.auth.checkToken :auth_token => self.user.flickr.access_token
     
+    # save the file temprarily locally
     temp_filename = "#{Rails.root}/tmp/photo_#{self.id}.img"
     uri = Domainatrix.parse(self.url)
-    
     Net::HTTP.start("static.gowalla.com") { |http|
       resp = http.get(uri.path)
       open(temp_filename, "wb") { |file|
@@ -20,7 +22,14 @@ class Photo < ActiveRecord::Base
       }
     }
     
-    flickr.auth.checkToken :auth_token => self.user.flickr.access_token
+    # upload the file from tmp folder
     flickr.upload_photo temp_filename
+    
+    # set the upload to true
+    uploaded = true
+    save
+    
+    #delete the file
+    File.delete(temp_filename)
   end
 end
