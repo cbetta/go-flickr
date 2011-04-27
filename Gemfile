fc75b5bad665e665dc6d737bb62e2e7c54aa96c7
Cristiano Betta <cbetta@gmail.com>
2011-04-27 23:57:29 +0200

    Fixes
    Signed-off-by: Cristiano Betta <cbetta@gmail.com>

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.6'
+gem 'rails', "3.0.6"
 gem "omniauth", :git => "git://github.com/cbetta/omniauth.git", :branch => "dependencyfix"
 gem "sqlite3-ruby"
 gem "gowalla", :git => "git://github.com/cbetta/gowalla.git", :branch => "ssl_fix"
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :gowalla, ENV['GWPHOTOS_GOWALLA_KEY'], ENV['GWPHOTOS_GOWALLA_SECRET'], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   provider :flickr, ENV['GWPHOTOS_FLICKR_KEY'], ENV['GWPHOTOS_FLICKR_SECRET'], :scope => "write"
 end
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
